304e911d3b5558f065230846217d4512f28e6bb1
roblesdotdev <httpsnip@gmial.com>
2023-02-25 13:42:56 -0300

    format with prettier

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,13 +2,13 @@ export default function Index() {
   return (
     <div
       style={{
-        minHeight: "95vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        minHeight: '95vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <h1>Working...</h1>
     </div>
-  );
+  )
 }
