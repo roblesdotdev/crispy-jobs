fe004dc8f4db867fe8034a17414c03970fcf0aed
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2023-02-26 23:44:56 -0300

    combobox search jobs (#4)

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,23 +1,11 @@
-import type { LoaderFunction } from '@remix-run/node'
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-import { getListing } from '~/utils/job.server'
-
-type LoaderData = {
-  jobs: Awaited<ReturnType<typeof getListing>>
-}
-
-export const loader: LoaderFunction = async () => {
-  const jobs = await getListing()
-  return json<LoaderData>({ jobs })
-}
+import { useSearchParams } from '@remix-run/react'
 
 export default function Index() {
-  const { jobs } = useLoaderData<LoaderData>()
+  const [searchParams] = useSearchParams()
 
   return (
     <div className="flex min-h-screen items-center justify-center text-xl">
-      <pre>{JSON.stringify(jobs, null, 2)}</pre>
+      <h1>Searching {searchParams.get('query')}</h1>
     </div>
   )
 }
