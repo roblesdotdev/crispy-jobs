1a43c27784e4c68217070f91b2d9011ab205e443
roblesdotdev <httpsnip@gmial.com>
2023-03-02 12:18:37 -0300

    email type

--- app/utils/email.server.ts
+++ app/utils/email.server.ts
@@ -3,19 +3,21 @@ import { getRequiredServerEnvVar } from './misc'
 const MAILGUN_SENDING_KEY = getRequiredServerEnvVar('MAILGUN_SENDING_KEY')
 const MAILGUN_DOMAIN = getRequiredServerEnvVar('MAILGUN_DOMAIN')
 
+type MailgunMessage = {
+  to: string
+  from: string
+  subject: string
+  text: string
+  html?: string | null
+}
+
 export async function sendEmail({
   to,
   from = 'example@email.com',
   subject,
   text,
   html,
-}: {
-  to: string
-  from?: string
-  subject: string
-  text: string
-  html?: string
-}) {
+}: MailgunMessage) {
   const auth = `${Buffer.from(`api:${MAILGUN_SENDING_KEY}`).toString('base64')}`
 
   if (!html) html = text
